210b9325be9834f8a8f8f5145c2f7fac1cb58b2f
fash1980 <115395492+fash1980@users.noreply.github.com>
2026-03-12 22:01:56 +0800

    Update app19.py

--- app19.py
+++ app19.py
@@ -714,7 +714,7 @@ with st.sidebar:
     #st.write("HF full response:", st.session_state.get("debug_hf_full_response", {}))
     
     st.write("Finish reason:", st.session_state.get("debug_finish_reason", "none"))
-    st.write("Message:", st.session_state.get("debug_message", {}))
+    #st.write("Message:", st.session_state.get("debug_message", {}))
     if st.session_state.debug_last:
         st.json(st.session_state.debug_last)
     st.session_state.strictness = st.slider("Strictness Level", 0, 3, 2)
@@ -1266,6 +1266,7 @@ elif st.session_state.step == "DONE":
                 pass
             st.session_state.clear()
             st.rerun()
+
 
 
 
