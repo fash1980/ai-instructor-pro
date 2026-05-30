13bb841b0d3647b1c880c4762ae51f3e4ada7507
fash1980 <115395492+fash1980@users.noreply.github.com>
2026-05-30 22:04:15 +0800

    Update app19.py

--- app19.py
+++ app19.py
@@ -400,7 +400,7 @@ def ollama_chat(messages, temperature=0.7, max_tokens=300):
     )
 
     model = genai.GenerativeModel(
-        "gemini-1.5-flash"
+        "gemini-2.0-flash"
     )
 
     prompt = "\n".join(
