0a53b6d4e9d1e0273566d97f27cc22ac9a0c806a
fash1980 <115395492+fash1980@users.noreply.github.com>
2026-05-30 20:50:06 +0800

    Update app19.py

--- app19.py
+++ app19.py
@@ -1155,7 +1155,7 @@ elif st.session_state.step == "COLLECT_PART":
             else:
                 # Stop timer + store submission safely
                 st.session_state.timer_started = False
-                st.session_state.pending_text = student_text
+                st.session_state.pending_text = text_for_checking
                 st.session_state.is_processing = True
                 st.rerun()
 
